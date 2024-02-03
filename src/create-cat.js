627fcf954a4fb470e79273d3982e1f0bfeb43593
Mohamed Mayallo <mayallo@baianat.com>
2024-02-03 07:15:34 +0200

    .

--- src/create-cat.js
+++ src/create-cat.js
@@ -1,7 +1,7 @@
 const createCat = async (catModel) => {
   const kitty = new catModel({
     name: 'Zildjian - ' + Math.random(),
-    age: 5
+    age: 4
   });
   await kitty.save();
   return kitty;
